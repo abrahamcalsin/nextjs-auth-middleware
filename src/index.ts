4bcccff5cdcf3ad1fb6227ee1c865223afb79ba8
abrahamcalsin <hello.abrahamcalsin@gmail.com>
2022-11-29 12:27:03 -0500

    feat: add auth middleware

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,45 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
-  }
-  return a + b;
-};
+import { NextApiRequest, NextApiResponse } from 'next';
+
+interface authMiddlewareOptions {
+  processEnv?: string | undefined;
+}
+
+async function authMiddleware(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  options: authMiddlewareOptions
+) {
+  const { processEnv = process.env.API_ROUTE_PUBLIC_KEY } = options;
+
+  return new Promise<void>((resolve, reject) => {
+    const key = 'x-api-public-key';
+
+    const value = req.headers[key] || req.query[key];
+
+    if (!value) {
+      return reject(
+        res.status(400).json({
+          error: {
+            message: 'The api public key is required',
+          },
+        })
+      );
+    }
+
+    if (value === processEnv) {
+      return resolve();
+    }
+
+    return reject(
+      res.status(401).json({
+        error: {
+          message: 'You are not authorized to call this api',
+        },
+      })
+    );
+  });
+}
+
+const NextAuthMiddleware = authMiddleware;
+
+export default NextAuthMiddleware;
